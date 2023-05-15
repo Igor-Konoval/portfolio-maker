78d8e6226e87a0c0a4826b7c4a75387d76e49bb1
Igor-Konoval <igorkonoval02@gmail.com>
2023-05-15 05:08:45 +0300

    storageModel v1

---
+++ GStorage/storageModel.js
@@ -0,0 +1,24 @@
+const Multer = require("multer");
+
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+      fileSize: 5 * 1024 * 1024,
+    },
+});
+
+let projectId = "direct-mason-384614";
+let keyFilename = 'myKey.json';
+
+const storage = new Storage({
+    projectId,
+    keyFilename,
+});
+
+const bucket = storage.bucket("img_bucket_trial"); // Get this from Google Cloud -> Storage
+
+module.exports = {
+    bucket,
+    storage,
+    multer
+}
