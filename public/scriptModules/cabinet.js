fd2c547ee7ae6e3ae4aeeae9932d9ca0e346fa9d
Igor-Konoval <igorkonoval02@gmail.com>
2023-05-27 22:03:09 +0300

    bugFix paste textNode

--- public/scriptModules/cabinet.js
+++ public/scriptModules/cabinet.js
@@ -4,34 +4,53 @@
 // const saveBtn = document.querySelector('.save-btn');
 
 // // function editValue(targ, index) {
-// //     let area = document.createElement('textArea');
-// //     area.className = "header_twosline_text_textArea";
-// //     targ.replaceWith(area);
-// //     area.value = relateValue[index];
-// //     area.addEventListener('changeArea', () => {
-// //         relateValue[index] = area.value;
-// //         area.replaceWith(targ);
-// //         targ.innerHTML = relateValue[index];
-// //     })
+// //   let area = document.createElement('textarea');
+// //   area.className = "header_twosline_text_textArea";
+// //   targ.replaceWith(area);
+// //   area.value = relateValue[index];
+// //   area.addEventListener('change', () => {
+// //     relateValue[index] = area.value;
+// //     area.replaceWith(targ);
+// //     targ.textContent = relateValue[index];
+// //   });
+// //   area.addEventListener('keydown', (event) => {
+// //     if (event.key === "Enter") {
+// //       event.preventDefault();
+// //       area.dispatchEvent(new Event('change'));
+// //     }
+// //   });
 // // }
 
-// function editValue(targ, index) {
+// function stripStyles(text) {
+//   const tempDiv = document.createElement('div');
+//   tempDiv.innerHTML = text;
+
+//   const allElements = tempDiv.getElementsByTagName('*');
+//   for (let i = 0; i < allElements.length; i++) {
+//     const element = allElements[i];
+//     element.removeAttribute('style');
+//     element.removeAttribute('class');
+//   }
+
+//   return tempDiv.innerText;
+// }
+
+// function editValue(targ) {
 //   let area = document.createElement('textarea');
 //   area.className = "header_twosline_text_textArea";
 //   targ.replaceWith(area);
-//   area.value = relateValue[index];
+//   area.value = targ.textContent;
 //   area.addEventListener('change', () => {
-//     relateValue[index] = area.value;
 //     area.replaceWith(targ);
-//     targ.innerHTML = relateValue[index];
-//   })
+//     targ.textContent = area.value;
+//   });
 //   area.addEventListener('keydown', (event) => {
 //     if (event.key === "Enter") {
 //       event.preventDefault();
+//       area.dispatchEvent(new Event('change'));
 //     }
 //   });
 // }
-
 
 // let allElemEdit = document.querySelectorAll('[contenteditable]');
 // allElemEdit = Array.from(allElemEdit).filter(el => el.offsetParent !== null);
@@ -43,16 +62,15 @@
 //   let inp = document.createElement('input');
 //   document.body.append(inp);
 //   inp.type = 'number';
-//   inp.className = 'f_size'
+//   inp.className = 'f_size';
 //   inp.style.display = 'none';
 //   inp.style.top = posT + 'px';
 //   inp.style.left = posL + 'px';
-  
+
 //   inp.addEventListener('change', (event) => {
 //     el.style.fontSize = inp.value + 'px';
 //   });
 // });
-
 
 // editBtn.addEventListener('click', () => {
 //   const editableElements = document.querySelectorAll('[contenteditable]');
@@ -60,32 +78,30 @@
 
 //   editableElements.forEach((el, index) => {
 //     el.setAttribute('contenteditable', 'true');
-
+    
 //     el.onclick = function(event) {
 //       if (!event.target.contenteditable) return false;
-//       relateValue[index] = event.target.innerHTML;
+//       relateValue[index] = event.target.innerText;
 //       editValue(event.target, index);
 //     }
 
 //     el.classList.add('editing');
 //   });
-  
+
 //   window.addEventListener('scroll', () => {
 //     editableElements.forEach((el, index) => {
-
 //       const posL = el.getBoundingClientRect().right + 10;
 //       const posT = el.getBoundingClientRect().top + el.clientHeight / 2 + window.pageYOffset;
-      
+
 //       if (editInputs[index].style.display === 'none') {
 //         return;
 //       }
-      
+
 //       editInputs[index].style.left = posL + 'px';
-//       editInputs[index].style.top = posT + 'px';  
-      
+//       editInputs[index].style.top = posT + 'px';
 //     });
 //   });
-  
+
 //   editableElements.forEach((el, index) => {
 //     const compSize = getComputedStyle(el);
 //     const posL = el.getBoundingClientRect().right + 10;
@@ -99,24 +115,26 @@
 //       editInputs[index].style.display = 'block';
 //       editInputs[index].value = parseInt(compSize.fontSize);
 //     }
-    
 //   });
 
 //   editBtn.setAttribute('disabled', true);
 // });
 
 // saveBtn.addEventListener('click', () => {
-//     const editableElements = document.querySelectorAll('[contenteditable="true"]');
-//     let editInputs = document.querySelectorAll('.f_size');
-//     editInputs.forEach(item => {
+//   const editableElements = document.querySelectorAll('[contenteditable="true"]');
+//   const editInputs = document.querySelectorAll('.f_size');
+
+//   editInputs.forEach(item => {
 //     item.style.display = 'none';
-//   })
-//     editableElements.forEach((el) => {
-//       el.setAttribute('contenteditable', 'false');
-//       el.classList.remove('editing');
-//       el.dispatchEvent(new Event('changeArea', {bubbles: true}));
-//     });
-//     editBtn.removeAttribute('disabled');
+//   });
+
+//   editableElements.forEach((el) => {
+//     el.setAttribute('contenteditable', 'false');
+//     el.classList.remove('editing');
+//     el.dispatchEvent(new Event('change'));
+//   });
+
+//   editBtn.removeAttribute('disabled');
 // });
 
 let relateValue = [];
@@ -124,15 +142,28 @@ let relateValue = [];
 const editBtn = document.querySelector('.edit-btn');
 const saveBtn = document.querySelector('.save-btn');
 
-function editValue(targ, index) {
+function stripStyles(text) {
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = text;
+
+  const allElements = tempDiv.getElementsByTagName('*');
+  for (let i = 0; i < allElements.length; i++) {
+    const element = allElements[i];
+    element.removeAttribute('style');
+    element.removeAttribute('class');
+  }
+
+  return tempDiv.innerText;
+}
+
+function editValue(targ) {
   let area = document.createElement('textarea');
   area.className = "header_twosline_text_textArea";
   targ.replaceWith(area);
-  area.value = relateValue[index];
+  area.value = targ.textContent;
   area.addEventListener('change', () => {
-    relateValue[index] = area.value;
     area.replaceWith(targ);
-    targ.innerHTML = relateValue[index];
+    targ.textContent = area.value;
   });
   area.addEventListener('keydown', (event) => {
     if (event.key === "Enter") {
@@ -169,9 +200,9 @@ editBtn.addEventListener('click', () => {
   editableElements.forEach((el, index) => {
     el.setAttribute('contenteditable', 'true');
 
-    el.onclick = function(event) {
+    el.onclick = function (event) {
       if (!event.target.contenteditable) return false;
-      relateValue[index] = event.target.innerHTML;
+      relateValue[index] = event.target.innerText;
       editValue(event.target, index);
     }
 
@@ -221,8 +252,23 @@ saveBtn.addEventListener('click', () => {
   editableElements.forEach((el) => {
     el.setAttribute('contenteditable', 'false');
     el.classList.remove('editing');
-    el.dispatchEvent(new Event('change'));
+
+    const cleanedText = stripStyles(el.textContent);
+    el.textContent = cleanedText;
   });
 
   editBtn.removeAttribute('disabled');
+});
+
+document.addEventListener('paste', (event) => {
+  const clipboardData = event.clipboardData || window.clipboardData;
+  const pastedText = clipboardData.getData('text/plain');
+
+  const cleanedText = stripStyles(pastedText);
+
+  const activeElement = document.activeElement;
+  if (activeElement && activeElement.getAttribute('contenteditable') === 'true') {
+    event.preventDefault(); 
+    document.execCommand('insertText', false, cleanedText);
+  }
 });
--- public/scriptModules/loadPostsFunction.js
+++ public/scriptModules/loadPostsFunction.js
@@ -1,63 +1,4 @@
 import { DataProvider } from './chanse.js'
-// // alert(DataProvider, ContentEditor);
-// class DataPostProvider extends DataProvider {
-//   postData() {
-//     return new Promise((resolve, reject) => {
-//       this.xhr.open('POST', this.url);
-//       this.xhr.onload = () => {
-//         if (this.xhr.status === 200) {
-//           alert(`Загружено: ${this.xhr.response}`);//response array imgs
-//           let dataImgs = this.xhr.response;//maybe trouble
-//           resolve(dataImgs);
-//         } else {
-//           reject('img error');
-//         }
-//       };
-//       this.xhr.send(this.fileImgs);
-//     })
-//   }
-
-//   // displayStaticImg(imgElem) {
-//   //   imgElem.style.visibility = 'visible';
-//   // }
-
-//   // findImgElements() {
-//   //   this.Imgs = document.querySelectorAll('img[src]');
-//   // }
-// }
-
-// class loadImgFiles {
-//   constructor(DataPostProvider) {
-//     this.DataPostProvider = DataPostProvider;
-//   }
-// }
-
-// class DataPostProvider extends DataProvider {
-//   postData() {
-//     return new Promise((resolve, reject) => {
-//       this.xhr.open('POST', this.url);
-//       this.xhr.onload = () => {
-//         if (this.xhr.status === 200) {
-//           alert(`Загружено: ${this.xhr.response}`);//response array imgs
-//           let dataImgs = this.xhr.response;//maybe trouble
-//           resolve(dataImgs);
-//         } else {
-//           reject('img error');
-//         }
-//       };
-//       this.xhr.send(this.fileImgs);
-//     })
-//   }
-
-//   displayStaticImg(imgElem) {
-//     imgElem.style.visibility = 'visible';
-//   }
-
-//   findImgElements() {
-//     this.Imgs = document.querySelectorAll('img[src]');
-//   }
-// }
-
 class GetImages {
   constructor(dataProvider) {
     this.dataProvider = dataProvider;
@@ -93,33 +34,6 @@ class GetImages {
 
 }
 
-// function displayStaticImg(imgElem) {
-//   imgElem.style.visibility = 'visible';
-// }
-
-// function loadPosts() {
-//     const loadImgs = document.querySelectorAll('img[src]');
-//     try {
-//       fetch(`/upload?usernameExport=${usernameExport}`)
-//       .then((res) => res.json())
-//       .then((x) => {
-//         for (let y = 0; y < x.length; y++) {
-//           let pos = x[y].split('_')[1];
-  
-//           loadImgs.forEach((item, index) => {
-//             if (index == pos) {
-//               item.setAttribute('src', `https://storage.googleapis.com/img_bucket_trial/${x[y]}`);
-//             }
-//             displayStaticImg(item);
-//           })
-//         }
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }   
-//   }
-
-
 document.addEventListener('DOMContentLoaded', function () {
   let dataProvider = new DataProvider(`/upload?usernameExport=${usernameExport}`);
   // let contentEditor = new ContentEditor(dataProvider);
@@ -139,5 +53,4 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-  // window.addEventListener('load', loadPosts);
 
